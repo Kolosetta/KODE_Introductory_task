8f6078433e4ed50b46c03c33b43366bb4ef4d784
tr00per <kolosovskiy91@gmail.com>
2022-09-21 21:49:18 +0300

    Добавлен первичный биндинг информации фрагмента детальной информации

--- app/src/main/java/com/example/kode_introductory_task/DetailInfoFragment.kt
+++ app/src/main/java/com/example/kode_introductory_task/DetailInfoFragment.kt
@@ -1,18 +1,25 @@
 package com.example.kode_introductory_task
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.example.kode_introductory_task.databinding.FragmentDetailInfoBinding
-import com.example.kode_introductory_task.databinding.FragmentUsersListBinding
 import com.example.kode_introductory_task.domain.Worker
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class DetailInfoFragment : Fragment() {
     private lateinit var binding: FragmentDetailInfoBinding
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -20,16 +27,30 @@ class DetailInfoFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val arguments = requireArguments()
         val fullName = StringBuilder(arguments.getString(FIRST_NAME)+ " ")
             .append(arguments.getString(LAST_NAME))
+        val fullNumber = StringBuilder("+7 ").append(arguments.getString(PHONE_NUMBER))
         binding.fullNameTv.text = fullName
         binding.meta.text = arguments.getString(TAG)
         binding.position.text = arguments.getString(POSITION)
-        binding.birthDate.text = arguments.getString(BIRTH_DATE)
-        binding.phoneNumber.text = arguments.getString(PHONE_NUMBER)
+        binding.phoneNumber.text = fullNumber
+
+        val date = arguments.getString(BIRTH_DATE) ?: "1970.01.01"
+        val simpleFormatter = SimpleDateFormat("yyyy-MM-dd").parse(date)
+        val textDate = DateFormat.getDateInstance(SimpleDateFormat.LONG,  Locale("ru"))
+            .format(simpleFormatter)
+
+        val periodFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val startDate = LocalDate.parse(date, periodFormatter)
+        val endDate = LocalDate.now()
+        val period = Period.between(startDate, endDate)
+        binding.age.text = period.years.toString()
+        binding.birthDate.text = textDate
     }
 
     companion object {
